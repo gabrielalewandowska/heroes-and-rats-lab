761ae2580d4a2ed2a4eb139946932a7af48e6069
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-10-14 09:25:48 +0100

    Finish Task and task_spec

--- specs/task_spec.js
+++ specs/task_spec.js
@@ -10,4 +10,16 @@ describe("Task Test", function(){
   it("has a difficulty level", function(){
     assert.strictEqual(slayADragon.difficultyLevel, 95);
   })
+
+  it("has an urgency level", function(){
+    assert.strictEqual(slayADragon.urgencyLevel, 60);
+  })
+
+  it("has a reward", function(){
+    assert.strictEqual(slayADragon.reward, "princess");
+  })
+
+  it("can be completed", function(){
+    assert.strictEqual(slayADragon.completed, false);
+  })
 })
