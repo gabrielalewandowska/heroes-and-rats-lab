3640d221049b88357e26663b511be75c0b753321
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-10-14 20:31:09 +0100

    Hero can view tasks by completion status

--- hero.js
+++ hero.js
@@ -9,4 +9,40 @@ Hero.prototype.talk = function () {
   return "Hi, I'm " + this.name;
 };
 
+Hero.prototype.eat = function (food) {
+  if(food.name === this.favouriteFood){
+    this.health += (food.replenishmentValue * 1.5);
+  } else {
+    this.health += food.replenishmentValue;
+  }
+};
+
+Hero.prototype.sortTasksByDifficulty = function (critrion) {
+  var sortedTasks = [];
+  sortedTasks = this.tasks.sort(function(a, b){
+    return b.difficultyLevel - a.difficultyLevel;
+  })
+  return sortedTasks;
+};
+
+Hero.prototype.sortTasksByUrgency = function () {
+  var sortedTasks = [];
+  sortedTasks = this.tasks.sort(function(a, b){
+    return b.urgencyLevel - a.urgencyLevel;
+  })
+  return sortedTasks;
+};
+
+Hero.prototype.filterTasksByReward = function (reward) {
+  return tasks = this.tasks.filter(function(item){
+    return item.reward === reward;
+  })
+};
+
+Hero.prototype.viewTasksByCompletion = function (value) {
+  return tasks = this.tasks.filter(function(item){
+    return item.completed === value;
+  })
+
+};
 module.exports = Hero;
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,11 +1,17 @@
 var assert = require('assert');
 var Hero = require('../hero.js');
+var Food = require('../food.js');
+var Task = require('../task.js');
 
 
 describe("Hero Test", function(){
 
   beforeEach(function(){
-    hero1 = new Hero("Steve", 100, "chocolate",[]);
+    slayADragon = new Task(95, 60, "princess", false);
+    stealGoldenApples = new Task(60, 40, "join the Argonauts", true);
+    killChimaera = new Task(73, 87, "get to ride the Pegasus", false);
+    hero1 = new Hero("Steve", 100, "chocolate",[slayADragon, stealGoldenApples, killChimaera]);
+    food1 = new Food("chocolate", 80);
   })
 
   it("has a name", function(){
@@ -21,10 +27,31 @@ describe("Hero Test", function(){
   })
 
   it("has tasks to complete", function(){
-    assert.deepEqual(hero1.tasks, []);
+    assert.deepEqual(hero1.tasks, [slayADragon, stealGoldenApples, killChimaera]);
   })
 
   it("can talk", function(){
     assert.strictEqual(hero1.talk(), "Hi, I'm Steve");
+  })
+
+  it("can eat and replenish their health", function(){
+    hero1.eat(food1);
+    assert.strictEqual(hero1.health, 220);
+  })
+
+  it("can sort tasks by difficulty", function(){
+    assert.deepEqual(hero1.sortTasksByDifficulty(), [slayADragon, killChimaera, stealGoldenApples]);
+  })
+
+  it("can sort tasks by urgency", function(){
+    assert.deepEqual(hero1.sortTasksByUrgency(), [killChimaera, slayADragon, stealGoldenApples]);
+  })
+
+  it("can sort tasks by reward", function(){
+    assert.deepEqual(hero1.filterTasksByReward("princess"), [slayADragon]);
+  })
+
+  it("can view tasks by completion", function(){
+    assert.deepEqual(hero1.viewTasksByCompletion(false), [slayADragon, killChimaera]);
   })
 })
